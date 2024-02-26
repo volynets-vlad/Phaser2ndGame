ef18be53cae5e349934f69c8ba53bc03c66cc44d
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-02-26 12:40:14 +0200

    /

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
     width: 1920,
-    height: 1080,
+    height: 1000,
     parent: game,
     physics: {
         default: 'arcade',
@@ -17,18 +17,54 @@ var config = {
     }
 };
 
+var game = new Phaser.Game(config);
+
 function preload()
 {
+    this.load.image('pes', 'assets/pes.png');
+    this.load.image('bg', 'assets/bg.png');
 
 }
 
 function create()
 {
+
     
+    this.add.image(500,500, 'bg');
+    player = this.physics.add.sprite(100, 450, 'pes');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+
 }
 
 function update()
 {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-250);
+
+      
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(250);
+
+        //player.anims.play('left', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
     
+    }
+
+    if (cursors.up.isDown/* && player.body.touching.down*/)
+    {
+        player.setVelocityY(-330);
+    }
 }
-document.getElementById('score').innerText()
+//document.getElementById('score').innerText()
