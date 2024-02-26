3480f46f3c3ebdc749ce9c5db199b18f36d2c140
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-02-26 11:59:36 +0200

    pochav

---
+++ game.js
@@ -0,0 +1,34 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    parent: game,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+function preload()
+{
+
+}
+
+function create()
+{
+    
+}
+
+function update()
+{
+    
+}
+document.getElementById('score').innerText()
