00b8f211d2f8cf3097632d20a19afb91fc25071e
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-03-11 12:55:26 +0200

    чота там падєлал

--- game.js
+++ game.js
@@ -18,36 +18,43 @@ var config = {
     }
 };
 
+var score = 0;
+var starStep = 100;
+var scoreText;
+var stars;
 var game = new Phaser.Game(config);
-var worldWidth = config.width*5;
+var worldWidth = config.width * 5;
 
-function preload()
-{
+function preload() {
     this.load.image('pes', 'assets/pes.png');
     this.load.image('bg', 'assets/bg.png');
     this.load.image('platform', 'assets/platform.png');
 
-    this.load.image('pen', 'assets/pen.jpg');
-    this.load.image('stone', 'assets/stone.jpg');
+    this.load.image('pen', 'assets/pen.png');
+    this.load.image('stone', 'assets/stone.png');
+    this.load.image('tree', 'assets/tree.png');
 
-    this.load.image('SP', 'assets/skyPlatform.jpg');
-    this.load.image('SPL', 'assets/skyPlatformL.jpg');
-    this.load.image('SPR', 'assets/skyPlatformR.jpg');   
+    this.load.image('SP', 'assets/skyPlatform.png');
+    this.load.image('SPL', 'assets/skyPlatformL.png');
+    this.load.image('SPR', 'assets/skyPlatformR.png');
+
+    this.load.image('star', 'assets/star.webp');
 }
 
-function create()
-{
+function create() {
 
+
+    this.add.tileSprite(0, 0, worldWidth, 1080, "bg")
+        .setOrigin(0, 0)
+        .setScale(1.5)
+        .setDepth(0);
     
-    this.add.tileSprite(0,0, worldWidth, 1080, "bg")
-        .setOrigin(0,0)
-        .setScale(1.5);
-    platforms = this.physics.add.staticGroup();
-   for(var x=0; x<worldWidth; x=x+128)
-    {
-        console.log(x)
-        platforms.create(x, 1080-128, 'platform').setOrigin(0,0).refreshBody();
         
+    platforms = this.physics.add.staticGroup();
+    for (var x = 0; x < worldWidth; x = x + 128) {
+        console.log(x)
+        platforms.create(x, 1080 - 128, 'platform').setOrigin(0, 0).refreshBody();
+
     }
     /*for (var x = 0; x < worldwidth; x = x + Phaser.Math. FloatBetween (400, 500)){
         var y = Phaser.Math.FloatBetween (100, 1000)
@@ -58,9 +65,9 @@ function create()
     player = this.physics.add.sprite(100, 450, 'pes');
 
     player
-    .setDepth(5)
-    .setBounce(0.2)
-    .setCollideWorldBounds(false);
+        .setDepth(5)
+        .setBounce(0.2)
+        .setCollideWorldBounds(false);
 
     this.physics.add.collider(platforms, player);
 
@@ -70,79 +77,127 @@ function create()
 
 
 
-    this.cameras.main.setBounds(0,0, worldWidth, 1080);
-    this.physics.world.setBounds(0,0, worldWidth,1080);
+    this.cameras.main.setBounds(0, 0, worldWidth, 1080);
+    this.physics.world.setBounds(0, 0, worldWidth, 1080);
     this.cameras.main.startFollow(player);
 
 
 
     stone = this.physics.add.staticGroup();
     pen = this.physics.add.staticGroup();
-    
-    for (var x = 0; x < worldWidth; x = x + Phaser.Math. FloatBetween (400, 700)) {
-    
+    tree = this.physics.add.staticGroup();
+
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(600, 1000)) {
+
         stone
-        .create(x, 1080 - 36*5, 'stone')
-        .setOrigin(0, 1)
-        .setScale (Phaser.Math.FloatBetween(10, 4, 2))
-        .setDepth (Phaser.Math.Between(1, 10));
-    console.log(stone.X, stone.Y)
+            .create(x, 1080 - 128, 'stone')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(3, 1.5, 0.2))
+            .setDepth(Phaser.Math.Between(1, 10));
+        console.log(stone.X, stone.Y)
     }
-    for (var x = 0; x < worldWidth; x = x + Phaser.Math. FloatBetween (500, 1000)) {
-    
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(900, 1500)) {
+
         pen
-        .create(x, 1080 - 36*5, 'pen')
-        .setOrigin(0, 1)
-        .setScale (Phaser.Math.FloatBetween(7, 3))
-        .setDepth (Phaser.Math.Between(1, 10));
-    console.log(pen.X, pen.Y)
+            .create(x, 1080 - 128, 'pen')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(3, .5))
+            .setDepth(Phaser.Math.Between(1, 10));
+        console.log(pen.X, pen.Y)
+    }
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(900, 1500)) {
+
+        tree
+            .create(x, 1080 - 128, 'tree')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(2, .5))
+            .setDepth(Phaser.Math.Between(1, 10));
+        console.log(tree.X, tree.Y)
     }
 
 
 
 
-    for (var x = 0; x < worldWidth; x = x + Phaser. Math. Between (800, 1000)) {
-        
-        var y = Phaser.Math.Between (100, 800)
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.Between(800, 1000)) {
+
+        var y = Phaser.Math.Between(100, 800)
 
         platforms
-        .create(x, y, 'SPL')
-        .setScale(4);
+            .create(x, y, 'SPL')
+            .setScale(1.5);
 
-        for(var i = 1; i < Phaser.Math.Between (1, 5); i++){
-        platforms.create(x + 36*4*i, y, 'SP')
-        .setScale(4); 
+        for (var i = 1; i < Phaser.Math.Between(1, 5); i++) {
+            platforms.create(x + 128 * 1.5 * i, y, 'SP')
+                .setScale(1.5);
         }
 
-        platforms.create(x + 36*4 * i, y, 'SPR')
-        .setScale(4);
-        }
+        platforms.create(x + 128 * 1.5 * i, y, 'SPR')
+            .setScale(1.5);
+    }
+
+    scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '90px', fill: 'black' })
+    .setOrigin(0,0)
+    .setScrollFactor(0)
+    .setDepth(1)
+    
+
+
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: worldWidth / starStep,
+        setXY: { x: 12, y: 0, stepX: starStep }
+    });
+    stars.children.iterate(function (child) {
+
+        child
+            .setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+            .setScale(0.3);
+
+    });
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
-function update()
-{
-    if (cursors.left.isDown)
-    {
+function update() {
+    if (cursors.left.isDown) {
         player.setVelocityX(-config.playerSpeed);
 
-      
+
     }
-    else if (cursors.right.isDown)
-    {
+    else if (cursors.right.isDown) {
         player.setVelocityX(config.playerSpeed);
 
         //player.anims.play('left', true);
     }
-    else
-    {
+    else {
         player.setVelocityX(0);
 
-    
+
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
+    if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-500);
     }
+
+
+
+
 }
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+    }
+}
+
+
 //document.getElementById('score').innerText()
